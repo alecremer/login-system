ab74a3d1bc20527aa12e44d0b14427253c1c0c06
Ale192837 <alexandre.2018@alunos.utfpr.edu.br>
2023-01-13 06:17:35 -0300

    home page template

--- src/presentation/view/Home.js
+++ src/presentation/view/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./Styles/home.css";
 
 export default class Home extends React.Component{
 
     render(){
         return(
             <div>
-                
+                <div className="top-bar">
+                    <div className="hello-text">
+                        Hello username!!
+                    </div>
+                </div>
             </div>
         )
     }
